196be0f50a3b61f1dd493b28a43d219b690ed0de
yum <richardminyu@foxmail.com>
2022-12-31 23:06:07 +0800

    refactor(lib/array): update

--- util/index.js
+++ util/index.js
@@ -3,22 +3,31 @@ const dataType = value => {
   let str = Object.prototype.toString.call(value);
   return str.slice(8, -1);
 };
+
 // 数值
 const isNumber = value => dataType(value) === 'Number';
+
 // 字符串
 const isString = value => dataType(value) === 'String';
+
 // 布尔
 const isBoolean = value => dataType(value) === 'Boolean';
+
 // 数组
 const isArray = value => dataType(value) === 'Array';
+
 // 对象
 const isObject = value => dataType(value) === 'Object';
+
 // 函数
 const isFunction = value => dataType(value) === 'Function';
+
 // 正则
 const isRegExp = value => dataType(value) === 'RegExp';
+
 // null
 const isNull = value => dataType(value) === 'Null';
+
 // NaN
 const isNaN = value => typeof value === 'number' && value !== value;
 
